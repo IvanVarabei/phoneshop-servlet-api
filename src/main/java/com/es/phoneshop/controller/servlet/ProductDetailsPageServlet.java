e65da74c02101e78f571420d7f060838362c46c8
IvanVarabei <garlic9991@gmail.com>
2020-07-21 17:40:20 +0300

    fixes from comments 2

--- src/main/java/com/es/phoneshop/controller/servlet/ProductDetailsPageServlet.java
+++ src/main/java/com/es/phoneshop/controller/servlet/ProductDetailsPageServlet.java
@@ -13,8 +13,7 @@ public class ProductDetailsPageServlet extends HttpServlet {
     private static final String PRODUCT_CODE_ATTRIBUTE = "product";
     private static final String ERROR_MESSAGE_ATTRIBUTE = "message";
     private static final String PRODUCT_DETAILS_PATH = "/WEB-INF/pages/productDetails.jsp";
-    private static final String ERROR_MESSAGE_PART_1 = "Product with code '";
-    private static final String ERROR_MESSAGE_PART_2 = "' not found.";
+    private static final String ERROR_MESSAGE_PART = "Product with code '%s' not found.";
     private static final int NOT_FOUND_ERROR_CODE = 404;
     private ArrayListProductDao arrayListProductDao = ArrayListProductDao.getInstance();
 
@@ -25,7 +24,7 @@ public class ProductDetailsPageServlet extends HttpServlet {
             request.setAttribute(PRODUCT_CODE_ATTRIBUTE, arrayListProductDao.findProduct(Long.valueOf(productId)));
             request.getRequestDispatcher(PRODUCT_DETAILS_PATH).forward(request, response);
         } catch (NumberFormatException | ItemNotFoundException e) {
-            request.setAttribute(ERROR_MESSAGE_ATTRIBUTE, ERROR_MESSAGE_PART_1 + productId + ERROR_MESSAGE_PART_2);
+            request.setAttribute(ERROR_MESSAGE_ATTRIBUTE, String.format(ERROR_MESSAGE_PART, productId));
             response.sendError(NOT_FOUND_ERROR_CODE);
         }
     }
--- src/main/webapp/WEB-INF/pages/error500.jsp
+++ src/main/webapp/WEB-INF/pages/error500.jsp
@@ -1,22 +1,7 @@
-<%
-    String message = pageContext.getException().getMessage();
-    String exception = pageContext.getException().getClass().toString();
-    StackTraceElement[] stackTrace = pageContext.getException().getStackTrace();
-%>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>500</title>
-</head>
-<body>
-<h2>Exception occurred while processing the request</h2>
-<p>Type: <%= exception%></p>
-<p>Message: <%= message %></p>
-<%
-    for (StackTraceElement stackTraceElement : stackTrace) {
-        out.println(stackTraceElement);
-    }
-%>
-</body>
-</html>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="tags" tagdir="/WEB-INF/tags" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<tags:master pageTitle="500">
+    <h1>Something went wrong.</h1>
+</tags:master>
