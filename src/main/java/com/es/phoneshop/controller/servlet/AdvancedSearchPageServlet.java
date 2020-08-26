ead1841cd9c2d074d0fc6c8a126b3bcdaa03c369
IvanVarabei <garlic9991@gmail.com>
2020-08-26 20:03:07 +0300

    refactor : refactor AdvSearch servlet v.2

--- src/main/java/com/es/phoneshop/controller/servlet/AdvancedSearchPageServlet.java
+++ src/main/java/com/es/phoneshop/controller/servlet/AdvancedSearchPageServlet.java
@@ -53,44 +53,4 @@ public class AdvancedSearchPageServlet extends HttpServlet {
         }
         req.getRequestDispatcher("/WEB-INF/pages/advancedSearch.jsp").forward(req, resp);
     }
-
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String productCode = req.getParameter("productCode");
-//        String potentialMinPrice = req.getParameter("minPrice");
-//        String potentialMaxPrice = req.getParameter("maxPrice");
-//        String potentialMinStock = req.getParameter("minStock");
-//        Map<String, String> searchErrors = new HashMap<>();
-//        Double minPrice = null;
-//        Double maxPrice = null;
-//        Integer minStock = null;
-//        if (potentialMinPrice != null && !potentialMinPrice.isEmpty()) {
-//            try {
-//                minPrice = Double.parseDouble(potentialMinPrice);
-//            } catch (NumberFormatException e) {
-//                searchErrors.put("minPriceError", "Not a number.");
-//            }
-//        }
-//        if (potentialMaxPrice != null && !potentialMaxPrice.isEmpty()) {
-//            try {
-//                maxPrice = Double.parseDouble(potentialMaxPrice);
-//            } catch (NumberFormatException e) {
-//                searchErrors.put("maxPriceError", "Not a number.");
-//            }
-//        }
-//        if (potentialMinStock != null && !potentialMinStock.isEmpty()) {
-//            try {
-//                minStock = Integer.parseInt(potentialMinStock);
-//            } catch (NumberFormatException e) {
-//                searchErrors.put("minStockError", "Not a number.");
-//            }
-//        }
-//        if (searchErrors.isEmpty()) {
-//            req.setAttribute("products", advancedSearchService
-//                    .searchAdvancedProducts(productCode, minPrice, maxPrice, minStock));
-//        } else {
-//            req.setAttribute("searchErrors", searchErrors);
-//        }
-//        req.getRequestDispatcher("/WEB-INF/pages/advancedSearch.jsp").forward(req, resp);
-//    }
 }
